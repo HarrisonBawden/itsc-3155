eecad9e06aa25aca841f874e5e5bc402531b2605
Harrison <bawdengithub@gmail.com>
2022-08-24 16:25:41 -0400

    First commit

---
+++ CloudComputing/ITCS3190_Activity1.1.py
@@ -0,0 +1,6 @@
+class leapYear():
+    year = int(input("Enter a year: "))
+    if ((year % 400 == 0) or ((year % 4 == 0))):
+        print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
