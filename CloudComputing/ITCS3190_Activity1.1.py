01807c0511dc0fa636809442754ef17309b693b3
HarrisonBawden <112023077+HarrisonBawden@users.noreply.github.com>
2022-08-24 16:29:23 -0400

    Delete CloudComputing directory

--- CloudComputing/ITCS3190_Activity1.1.py
+++
@@ -1,6 +0,0 @@
-class leapYear():
-    year = int(input("Enter a year: "))
-    if ((year % 400 == 0) or ((year % 4 == 0))):
-        print(year, "is a leap year")
-    else:
-        print(year, "is not a leap year")
